0c6e906156dd490a3cbc4065a4c2314d33274114
Steven Epstein <sbepstein42@gmail.com>
2021-11-13 16:59:05 -0500

    Part 1 Completed

--- process.py
+++ process.py
@@ -1,12 +1,11 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") # opens the file specified in quatation marks
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): # defines function sales_reports with input (log_file)
+    for line in log_file: # iterates through each line in the file opened on line 1
+        line = line.rstrip() # strips white space off of each line
+        day = line[0:3] # between cursor position 0 and (spaced in) 3, focuses on the characters between those positions = first 3 letter on each line
+        if day == "Mon": # if those first 3 letters on each line == "Tue"
+            print(line) # then print the entire line
 
-
-sales_reports(log_file)
+sales_reports(log_file) # this invokes the function outlined above, which while will print each line starting with the 3 characters matching what is specified on line 8.
